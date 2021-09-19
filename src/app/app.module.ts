57e90343055f40b2aa2fd11f7189bd0d595b3d9d
Trijun <trijun.mj@gmail.com>
2021-09-19 10:46:12 +0530

    adding files of login and register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 // forms module added for using ngModel
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,8 @@ import { Day1Component } from './day1/day1.component';
 import { Day2Component } from './day2/day2.component';
 import { Day3Component } from './day3/day3.component';
 import { Day4Component } from './day4/day4.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { Day4Component } from './day4/day4.component';
     Day1Component,
     Day2Component,
     Day3Component,
-    Day4Component
+    Day4Component,
+    LoginPageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/register-page/register-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.css']
+})
+export class RegisterPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  onSubmit(userForm: any){
+  console.log(userForm.value);
+  }
+
+}
